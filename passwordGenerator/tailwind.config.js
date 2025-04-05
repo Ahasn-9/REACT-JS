64a561a3eda2ebab77a679a49921b71ccc4c0653
Ahasn-9 <mughalahsan718@gmail.com>
2025-04-05 15:15:26 +0500

    setting envirnment

---
+++ passwordGenerator/tailwind.config.js
@@ -0,0 +1,10 @@
+module.exports = {
+  content: [
+    "./src/**/*.{html,js}", // Adjust the paths based on your project structure
+    "./public/index.html",
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
